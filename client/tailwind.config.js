bf13422834d70fb532d29cb4bb5c5c9ca28e128c
abderrazzaq mars <abderrazzaqmars99@gmail.com>
2023-01-23 15:18:17 +0100

    Add initial files

---
+++ client/tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      green: colors.green,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red,
+      green: colors.green,
+    },
+    fontFamily: {
+      main: ['Roboto', 'sans-serif'],
+      mini: ['Montserrat', 'sans-serif'],
+    }
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: ["light"],
+    base: false,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+}
