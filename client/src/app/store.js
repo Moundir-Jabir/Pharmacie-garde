1db892737e5c4fab86d308beb3a1d5c6b7ac5521
Elboukhari-Elkassmi-Mohamed <m.elboukhari.elkassmi@hotmail.com>
2023-01-24 14:14:24 +0100

    redux

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,15 @@
-import {configureStore} from "@reduxjs/toolkit"
 
+import { configureStore } from "@reduxjs/toolkit";
+import item, {fetchItems, postItem, putItem, deleteItemApi} from "./itemSlice";
 
-const store = configureStore ({
-    reducer: {}
-})
+const store = configureStore({
+  reducer: {
+    item
+  }
+});
 
-export default store
+export const {dispatch} = store;
+
+export const {fetchItems, postItem, putItem, deleteItemApi} = {fetchItems, postItem, putItem, deleteItemApi}
+
+export default store;
